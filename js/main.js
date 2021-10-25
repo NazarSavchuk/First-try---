97f4969386d5aeb0e72aab1c458e3d15b31d8b2e
NazarSavchuk <nazarsavcuk3@gmail.com>
2021-10-25 22:05:31 +0300

    It's my own disigned web site (It's first page what I have created in GitHub pages

---
+++ js/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $('.slider').slick({
+        arrows:true,
+        dots:true,
+        adaptiveHeight:true,
+        slidesToShow:1,
+        slidesToScroll:1,
+        speed:300,
+        autoplay:true,
+        autoplaySpeed:3000,
+        
+    });
+})
