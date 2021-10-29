cbf5b569d3556a286c8c8314d86579f735673c5b
NazarSavchuk <nazarsavcuk3@gmail.com>
2021-10-29 16:46:03 +0300

    add scroll effects with pure JS

--- js/main.js
+++ js/main.js
@@ -11,4 +11,22 @@ $(document).ready(function(){
         
     });
 })
-AOS.init();
+
+document.addEventListener('DOMContentLoaded', () => {
+    const mainSlider = document.querySelector('.slider');
+    const header = document.querySelector('.header');
+    window.addEventListener('scroll', () => {
+        let scrollTop = window.scrollY;
+        let mainSliderCenter = mainSlider.offsetHeight / 2;
+        console.log(mainSliderCenter);
+        console.log(scrollTop);
+
+        if (scrollTop >= mainSliderCenter){
+            header.classList.add('fixed');
+            mainSlider.style.marginTop = `${header.offsetHeight}px`;
+        } else {
+            header.classList.remove('fixed');
+            mainSlider.style.marginTop = `0px`;
+        }
+    });
+});
