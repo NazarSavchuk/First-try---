f9d34aabe75ffb2bbba2ea6ba0b1ffa9cc2ca03d
NazarSavchuk <nazarsavcuk3@gmail.com>
2021-10-30 15:11:54 +0300

    Adding new styles and simple animations on JS. You can fix the slider problems if you want:)

--- js/main.js
+++ js/main.js
@@ -13,20 +13,45 @@ $(document).ready(function(){
 })
 
 document.addEventListener('DOMContentLoaded', () => {
-    const mainSlider = document.querySelector('.slider');
-    const header = document.querySelector('.header');
-    window.addEventListener('scroll', () => {
-        let scrollTop = window.scrollY;
-        let mainSliderCenter = mainSlider.offsetHeight / 2;
-        console.log(mainSliderCenter);
-        console.log(scrollTop);
+        const mainSlider = document.querySelector('.slider');
+        const header = document.querySelector('.header');
+        const scrollAnimateItem = document.querySelectorAll('.scrollAnimateItem');
 
-        if (scrollTop >= mainSliderCenter){
-            header.classList.add('fixed');
-            mainSlider.style.marginTop = `${header.offsetHeight}px`;
-        } else {
-            header.classList.remove('fixed');
-            mainSlider.style.marginTop = `0px`;
+
+        const scrollAnimation = () => {
+            let windowPoint = (window.innerHeight * 0.9) + window.scrollY;
+            console.log(windowPoint);
+            scrollAnimateItem.forEach(el => {
+                let scrollOffset = el.offsetTop + (el.offsetHeight * 0.2);
+                if (windowPoint >= scrollOffset) {
+                    el.classList.add('animation-class');
+                } else {
+                    el.classList.remove('animation-class');
+                }
+            });
+        };
+
+
+        const headerFixed = () => {
+            let scrollTop = window.scrollY;
+            let mainSliderCenter = mainSlider.offsetHeight / 2;
+            if (scrollTop >= mainSliderCenter){
+                header.classList.add('fixed');
+                mainSlider.style.marginTop = `${header.offsetHeight}px`;
+            } else {
+                header.classList.remove('fixed');
+                mainSlider.style.marginTop = `0px`;
+            }
         }
-    });
+
+        headerFixed();
+        scrollAnimation();
+        window.addEventListener('scroll', () => {
+            headerFixed();        
+            scrollAnimation();    
+        });
 });
+
+
+
+
